56bdabec5438af72ffeb92d2302813327ddc97fc
melvinioso <matt.mcgavern@gmail.com>
2020-05-31 22:45:27 -0500

    Implemented stress testing on proxy

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
@@ -7,24 +8,24 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/api/items/:id', createProxyMiddleware({
-  target: 'http://3.132.5.204:3001/',
-  changeOrigin: true,
-}));
+// app.use('/api/items/:id', createProxyMiddleware({
+//   target: 'http://3.132.5.204:3001/',
+//   changeOrigin: true,
+// }));
 
 app.use('/items/:id', createProxyMiddleware({
-  target: 'http://34.201.53.74:3002/',
+  target: 'http://localhost:3002/',
   changeOrigin: true,
 }));
 
-app.use('/api/related_products/:id', createProxyMiddleware({
-  target: 'http://54.166.182.193:3003/',
-  changeOrigin: true,
-}));
+// app.use('/api/related_products/:id', createProxyMiddleware({
+//   target: 'http://54.166.182.193:3003/',
+//   changeOrigin: true,
+// }));
 
-app.use('/api/allreviews', createProxyMiddleware({
-  target: 'http://18.212.184.37:3004/',
-  changeOrigin: true,
-}));
+// app.use('/api/allreviews', createProxyMiddleware({
+//   target: 'http://18.212.184.37:3004/',
+//   changeOrigin: true,
+// }));
 
 app.listen(port);
